eccc26a7fd295c68b5717a30fcd29b3ee34d568a
Sergey Matkin <mserg-90@mail.ru>
2022-05-17 22:16:15 +0300

    4 - Урок 8

--- src/main/java/alishevCours/RockMusic.java
+++ src/main/java/alishevCours/RockMusic.java
@@ -1,6 +1,21 @@
 package alishevCours;
 
 public class RockMusic implements Music {
+
+    private RockMusic() {};
+
+    public static RockMusic getRockMusic() {
+        return new RockMusic();
+    }
+
+    public void doMyInit() {
+        System.out.println("Doing my initialization");
+    }
+
+    public void doMyDestroy() {
+        System.out.println("Doing my destruction");
+    }
+
     @Override
     public String getSong() {
         return "Russian dream";
--- src/main/java/alishevCours/TestSpring.java
+++ src/main/java/alishevCours/TestSpring.java
@@ -8,15 +8,31 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
+        RockMusic rockMusic = context.getBean("rockMusicBean", RockMusic.class);
+        System.out.println(rockMusic.getSong());
+
+        RockMusic rockMusic1 = context.getBean("rockMusicBean", RockMusic.class);
+
+//        MusicPlayer firstMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        MusicPlayer secondMusicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//
+//        boolean comparison = firstMusicPlayer == secondMusicPlayer;
+//        System.out.println(comparison);
+//
+//        firstMusicPlayer.setVolume(10);
+//
+//        System.out.println(firstMusicPlayer.getVolume());
+//        System.out.println(secondMusicPlayer.getVolume());
+
 //        Music music = context.getBean("musicBean", Music.class);
 //
 //        MusicPlayer musicPlayer = new MusicPlayer(music);
 
-        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
-        musicPlayer.playMusic();
-        System.out.println(musicPlayer.getName());
-        System.out.println(musicPlayer.getVolume());
-        musicPlayer.playMusicList();
+//        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+//        musicPlayer.playMusic();
+//        System.out.println(musicPlayer.getName());
+//        System.out.println(musicPlayer.getVolume());
+//        musicPlayer.playMusicList();
 
         context.close();
     }
